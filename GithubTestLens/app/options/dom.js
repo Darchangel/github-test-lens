38f22844098b93a7c6be99f22772737e5eb29d65
Filipe Silva <filipe.av.silva@gmail.com>
2016-05-11 00:54:00 +0200

    Fix bug when removing regex rows in the options.
    A dumb way of creating 'unique' IDs for the rows (using the current DateTime's timestamp)
    led to that, when multiple rows were loaded, 2 or more rows would get the same ID.
    This would in turn cause that the wrong (the first one with matching ID) row was removed when
    the 'Remove' button was pressed.
    
    This commit fixes that by generating sane IDs (an incrementing integer) for the rows

--- GithubTestLens/app/options/dom.js
+++ GithubTestLens/app/options/dom.js
@@ -3,126 +3,129 @@ GithubTestLens.options = GithubTestLens.options || {};
 
 var defaults = GithubTestLens.options.defaults;
 
-GithubTestLens.options.dom = (function() {
+GithubTestLens.options.dom = (function () {
 
-var optionsTableId = "optionsTable";
+    var optionsTableId = "optionsTable";
+    var idCounter = 1;
 
-function setupOptionsTable(options) {
-    var table = document.getElementById(optionsTableId);
-    var body = table.tBodies[0];
-
-    body.innerHTML = "";
-
-    options.forEach(regexPair => {
-        var row = createRow(regexPair, "regexrow_" + new Date().getTime()); 
-        appendRowToOptionsTable(row);
-    });
-}
-
-function appendRowToOptionsTable(row) {
-    var table = document.getElementById(optionsTableId);
-    var body = table.tBodies[0];
-
-    body.appendChild(row);
-}
-
-function createRow(regexTriple, id) {
-    var row = document.createElement("tr");
-    row.id = id;
-
-    row.appendChild(createInputCell(regexTriple.name));
-    row.appendChild(createInputCell(regexTriple.fileRegex));
-    row.appendChild(createInputCell(regexTriple.testRegex));
-
-    var buttonCell = document.createElement("td");
-    buttonCell.appendChild(createRemoveButton(id));
-    row.appendChild(buttonCell);
-
-    return row;
-}
-
-function createNewRow(id) {
-    return createRow({
-        name: "",
-        fileRegex: "",
-        testRegex: ""
-    }, id);
-}
-
-function createRemoveButton(rowId) {
-    var btn = document.createElement("button");
-    var text = document.createTextNode("Remove");
-    btn.appendChild(text);
-    btn.onclick = () => removeElement(rowId);
-
-    return btn;
-}
-
-function removeElement(id) {
-    var elm = document.getElementById(id);
-    elm.parentNode.removeChild(elm);
-}
-
-function createInputCell(text) {
-    var cell = document.createElement("td");
-    var inputBox = document.createElement("input");
-    inputBox.type = "text";
-    inputBox.value = text;
-
-    cell.appendChild(inputBox);
-    return cell;
-}
-
-function addRegexRow() {
-    var row = createNewRow(String(new Date().getTime()));
-    appendRowToOptionsTable(row);
-}
-
-function parseOptionsFromTable() {
-    var table = document.getElementById(optionsTableId);
-    var body = table.tBodies[0];
-
-    var cells = body.getElementsByTagName("td");
-    var regexps = gatherRegexps(cells);
-    var pairs = createRegexPairs(regexps);
-
-    return pairs;
-}
-
-function gatherRegexps(cells) {
-    var regexps = [];
-
-    for (var i = 0; i < cells.length; ++i) {
-        var cell = cells[i];
-        var value = getValueFromCell(cell);
-        if(value)
-            regexps.push(value);
+    function createUniqueRowId() {
+        return "regexrow_" + idCounter++;
     }
 
-    return regexps;
-}
+    function setupOptionsTable(options) {
+        var table = document.getElementById(optionsTableId);
+        var body = table.tBodies[0];
 
-function createRegexPairs(regexps) {
-    var pairs = [];
-    for (var i = 0; i < regexps.length; i+=3) {
-        pairs.push({
-            name: regexps[i],
-            fileRegex: regexps[i+1],
-            testRegex: regexps[i+2]
+        body.innerHTML = "";
+
+        options.forEach(regexPair => {
+            var row = createRow(regexPair);
+            appendRowToOptionsTable(row);
         });
     }
 
-    return pairs;
-}
+    function appendRowToOptionsTable(row) {
+        var table = document.getElementById(optionsTableId);
+        var body = table.tBodies[0];
 
-function getValueFromCell(cell) {
-    var result = null;
-    var text = cell.getElementsByTagName("input")[0];
-    if(text)
-        result = text.value;
+        body.appendChild(row);
+    }
 
-    return result;
-}
+    function createRow(regexTriple) {
+        var id = createUniqueRowId();
+        var row = document.createElement("tr");
+        row.id = id;
+
+        row.appendChild(createInputCell(regexTriple.name));
+        row.appendChild(createInputCell(regexTriple.fileRegex));
+        row.appendChild(createInputCell(regexTriple.testRegex));
+
+        var buttonCell = document.createElement("td");
+        buttonCell.appendChild(createRemoveButton(id));
+        row.appendChild(buttonCell);
+
+        return row;
+    }
+
+    function createRemoveButton(rowId) {
+        var btn = document.createElement("button");
+        var text = document.createTextNode("Remove");
+        btn.appendChild(text);
+        btn.onclick = () => removeElement(rowId);
+
+        return btn;
+    }
+
+    function removeElement(id) {
+        var elm = document.getElementById(id);
+        elm.parentNode.removeChild(elm);
+    }
+
+    function createInputCell(text) {
+        var cell = document.createElement("td");
+        var inputBox = document.createElement("input");
+        inputBox.type = "text";
+        inputBox.value = text;
+
+        cell.appendChild(inputBox);
+        return cell;
+    }
+
+    function addRegexRow() {
+        var row = createRow({
+            name: "",
+            fileRegex: "",
+            testRegex: ""
+        });
+
+        appendRowToOptionsTable(row);
+    }
+
+    function parseOptionsFromTable() {
+        var table = document.getElementById(optionsTableId);
+        var body = table.tBodies[0];
+
+        var cells = body.getElementsByTagName("td");
+        var regexps = gatherRegexps(cells);
+        var pairs = createRegexPairs(regexps);
+
+        return pairs;
+    }
+
+    function gatherRegexps(cells) {
+        var regexps = [];
+
+        for (var i = 0; i < cells.length; ++i) {
+            var cell = cells[i];
+            var value = getValueFromCell(cell);
+            if (value)
+                regexps.push(value);
+        }
+
+        return regexps;
+    }
+
+    function createRegexPairs(regexps) {
+        var pairs = [];
+        for (var i = 0; i < regexps.length; i += 3) {
+            pairs.push({
+                name: regexps[i],
+                fileRegex: regexps[i + 1],
+                testRegex: regexps[i + 2]
+            });
+        }
+
+        return pairs;
+    }
+
+    function getValueFromCell(cell) {
+        var result = null;
+        var text = cell.getElementsByTagName("input")[0];
+        if (text)
+            result = text.value;
+
+        return result;
+    }
 
     return {
         setupOptionsTable: setupOptionsTable,
